d78a10f97f0a8384fdd0cd0567aca7bad75e4f92
Owen Eaves <owenjeaves@gmail.com>
2024-02-06 14:30:42 +0000

    Edit update function

--- routes/index.js
+++ routes/index.js
@@ -35,91 +35,93 @@ router.get("/update", asyncHandler(async function (req, res, next) {
 
   await fetchSeasonData(url, season)
 
+
+  async function fetchSeasonData(url, season){
+    await axios(url)
+    .then(async (response) => {
+        const allMatches = []
+        const html = response.data
+        const $ = cheerio.load(html)
+         $(".standard_tabelle").find("a").each(async function(){
+            const text = $(this).text()
+            if (text.length === 10 && text[text.length - 2] === ")"){
+                const href = $(this).attr("href")
+                const matchURL = `https://www.worldfootball.net${href}`
+                const matchData = fetchMatchData(matchURL, season)
+                //console.log(matchData)
+                allMatches.push(matchData)
+            }
+        })
+        return allMatches
+  
+    })
+    .then(async (response) => {
+        const data =  await Promise.all(response)
+        console.log(data)
+        for (const match of data){
+          const newMatch = new Match({
+            season: match.season,
+            homeTeam: match.homeTeam,
+            awayTeam: match.awayTeam,
+            score: match.score,
+            awayGoalScorers: match.awayGoalScorers,
+            stadium: match.stadium
+          })
+          await newMatch.save()
+        }
+        res.json(data)
+    })
+  }
+  
+  
+  async function fetchMatchData(url, season){
+    const matchData = {season}
+    await axios(url)
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const teams = $("[width=35%]").each(function(i){
+                const team = $(this).text().trim()
+                if (i === 0){
+                    matchData.homeTeam = team
+                } else{
+                    matchData.awayTeam = team
+                }
+            })
+            const score = $(".resultat").text().trim()
+            matchData.score = score
+            const stadium = $("[href^=/venues]").text().slice(8)
+            matchData.stadium = stadium
+            matchData.awayGoalScorers = []
+            const awayGoalScorers = $("td[style='padding-left: 50px;']").each(function(){
+                const scorerData = $(this).text()
+                const words = scorerData.slice(1).split(" ")
+                let name = ""
+                let minute
+                for (const word of words){
+                    if (word.match(/^\d/)) {
+                        minute = word.replace(".", "'")
+                        name = name.slice(0, -1)
+                        break
+                     }
+                     else{
+                        name += word + " "
+                     }
+                }
+                matchData.awayGoalScorers.push({
+                    name,
+                    minute
+                })
+            })
+           // console.log(matchData)
+            return matchData
+        })
+        return matchData
+  }
+  
+  
 }))
 
-
-async function fetchSeasonData(url, season){
-  await axios(url)
-  .then(async (response) => {
-      const allMatches = []
-      const html = response.data
-      const $ = cheerio.load(html)
-       $(".standard_tabelle").find("a").each(async function(){
-          const text = $(this).text()
-          if (text.length === 10 && text[text.length - 2] === ")"){
-              const href = $(this).attr("href")
-              const matchURL = `https://www.worldfootball.net${href}`
-              const matchData = fetchMatchData(matchURL, season)
-              //console.log(matchData)
-              allMatches.push(matchData)
-          }
-      })
-      return allMatches
-
-  })
-  .then(async (response) => {
-      const data =  await Promise.all(response)
-      console.log(data)
-      for (const match of data){
-        const newMatch = new Match({
-          season: match.season,
-          homeTeam: match.homeTeam,
-          awayTeam: match.awayTeam,
-          score: match.score,
-          awayGoalScorers: match.awayGoalScorers,
-          stadium: match.stadium
-        })
-        await newMatch.save()
-      }
-      
-  })
-}
-
-
-async function fetchMatchData(url, season){
-  const matchData = {season}
-  await axios(url)
-      .then((response) => {
-          const html = response.data
-          const $ = cheerio.load(html)
-          const teams = $("[width=35%]").each(function(i){
-              const team = $(this).text().trim()
-              if (i === 0){
-                  matchData.homeTeam = team
-              } else{
-                  matchData.awayTeam = team
-              }
-          })
-          const score = $(".resultat").text().trim()
-          matchData.score = score
-          const stadium = $("[href^=/venues]").text().slice(8)
-          matchData.stadium = stadium
-          matchData.awayGoalScorers = []
-          const awayGoalScorers = $("td[style='padding-left: 50px;']").each(function(){
-              const scorerData = $(this).text()
-              const words = scorerData.slice(1).split(" ")
-              let name = ""
-              let minute
-              for (const word of words){
-                  if (word.match(/^\d/)) {
-                      minute = word.replace(".", "'")
-                      name = name.slice(0, -1)
-                      break
-                   }
-                   else{
-                      name += word + " "
-                   }
-              }
-              matchData.awayGoalScorers.push({
-                  name,
-                  minute
-              })
-          })
-         // console.log(matchData)
-          return matchData
-      })
-      return matchData
-}
 
 
 
